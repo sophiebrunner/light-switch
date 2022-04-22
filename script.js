6f1caf2eaedf8d81afdadf4998bf89ca44613d73
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-04-22 13:59:45 +0200

    feat: add if-condition to change the site title depending on the status of the light-switch

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
-let title = document.querySelector("title");
 
 const toggleLightSwitch = function () {
   button.classList.toggle("button--night");
   body.classList.toggle("body--night");
-};
-
-const goodMorning = function () {
-  title = "Good Morning";
-};
-
-const goodNight = function () {
-  title = "Good Night";
+  if (button.classList.contains("button--night")) {
+    document.title = "Good Night";
+  } else {
+    document.title = "Good Morning";
+  }
 };
 
 button.addEventListener("click", toggleLightSwitch);
-button.addEventListener("click", goodMorning);
-button.addEventListener("click", goodMorning);
